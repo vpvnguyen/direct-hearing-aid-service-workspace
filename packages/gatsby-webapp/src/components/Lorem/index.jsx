98a007fc274b4bd08682821f1d696de66a81b71b
vpvnguyen <vpvnguyen@gmail.com>
2024-07-16 04:06:56 -0700

    wip: page content

--- packages/gatsby-webapp/src/components/Lorem/index.jsx
+++ packages/gatsby-webapp/src/components/Lorem/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const lorem = `
 <h1>Lorem Ipsum</h1>
@@ -55,7 +55,395 @@ Himenaeos bibendum fames vehicula tincidunt fringilla platea felis platea! Urna 
 
 `;
 
+// const Lorem = () => <div style={{ paddingTop: '6rem' }} dangerouslySetInnerHTML={{ __html: lorem }} />
+const Page1 = () => {
+  return (
+    <div>
+      <h1>Page 1</h1>
+      <p>
+        Most Hearing Aids can be repaired regardless of age to save you money.
+      </p>
+      <p>
+        Offers 48-hour "in-house" turnaround on most hearing aid repair so you
+        can get your Hearing back faster.
+      </p>
+      <p>Provides new faceplates and upgrades on most custom models.</p>
+      <p>
+        Re-casing available on ALL custom models to extend the life of your
+        Hearing Aid.
+      </p>
+      <p>
+        SERVICE: Offers refurbished BTE (Behind the Ear) Hearing Aids for just
+        $495.00 with One-year Guarantee.
+      </p>
+      <p>
+        Extended "Worry-Free" Warranty on our hearing aid repair service
+        available for an additional six months - $129.99
+      </p>
+      <h2>Services</h2>
+      <p>
+        At Direct Hearing Aid Repair California, we understand the importance of
+        finding the hearing aid that is right for you. Our factory trained and
+        certified technicians make sure that every repair is done correctly the
+        first time. We also have a support team available to assist you with all
+        questions and needs that arise.{" "}
+      </p>
+      <h3> Our services include:</h3>
+      <ul>
+        <li>24-48 hour turnaround on most repairs</li>
+        <li>
+          Factory trained & certified technicians with 20+ years of experience
+          in hearing aid repair service
+        </li>
+        <li>
+          Support services to help our customers make sure they are getting fit
+          for their needs
+        </li>
+        <li>Quality and timely repairs</li>
+        <li>Same-day pick-up for the Los Angeles and Orange County areas</li>
+        <li>Custom Re-Shell</li>
+        <li>Custom Re-casing</li>
+        <li>Circuit Upgrades from analog to digital</li>
+        <li>Circuit repair</li>
+        <li>Walk-in cleaning and maintenance check</li>
+        <li>Direct shipping</li>
+      </ul>
+      <p>
+        Direct Hearing Aid offers the highest quality hearing aid repair and
+        parts available. With our support team, you can rest assured that you
+        will be fit with the product that is right for you.
+      </p>
+    </div>
+  );
+};
 
-const Lorem = () => <div style={{ paddingTop: '6rem' }} dangerouslySetInnerHTML={{ __html: lorem }} />
+const Page2 = () => (
+  <div>
+    <h1>Page 2</h1>
+    <h2>Choosing A Hearing Aid Repair Lab</h2>
+    <h2>Where Should You Have Your Hearing Aid Repaired ?</h2>
+    <p>
+      This is a question most hearing aid users will have to ask themselves
+      sooner or later.
+    </p>
+    <p>
+      Hearing aids are hybrids of mechanical and electronic devices. They are
+      subject to moisture, dust, dirt, earwax, debris, and physical abuse and
+      handling, and at some point in time, will require maintenance and
+      possibly, major or minor repair.
+    </p>
+    <p>
+      <b>
+        As a hearing aid user, it is important you understand that the basic
+        purpose of hearing aid manufacturers and retail outlets is to
+        continually sell you new product.
+      </b>{" "}
+      Why? That’s how they make the greatest profit, so these companies try to
+      deter clients from repairing their hearing aids and instead pressure them
+      to buy a new one. This attitude is reflected by many outlets who claim
+      they cannot repair even their own brand of hearing aid after a specified
+      period of time, such as 3 or 5 years.
+    </p>
+    <p>
+      <b>All hearing aids, standardized or custom-made, can be repaired.</b>{" "}
+      Whether you need to fix the microphone, receiver, a broken wire, or a bad
+      transistor, it can be repaired.
+    </p>
+    <p>
+      <b>So, who should repair your hearing aid?</b>
+      The best source for hearing aid repair is someone that repairs,
+      recondition, hearing aids with warranty.
+    </p>
+    <p>
+      <b>
+        An independent hearing aid repair lab is dedicated to providing all the
+        services, options, and products, necessary to maximize the performance
+        of your hearing aid at the most reasonable price.
+      </b>
+    </p>
+    <p>
+      Independent hearing aid labs:
+      <ul>
+        <li>
+          Have no ulterior motives in representing what can or cannot be
+          repaired on an existing hearing aid.
+        </li>
+        <li>
+          Guarantee the electronic performance of the hearing aids they repair.
+        </li>
+        <li>
+          Want the hearing aid to perform perfectly once it has been repaired.
+        </li>
+      </ul>
+    </p>
+    <p>
+      As a consumer, it is important for you to know the following about the
+      technicians who will be repairing your hearing aid:
+    </p>
+    <ul>
+      <li>The qualifications of the repair lab and their technicians</li>
+      <li>The repair lab’s reputation within the market</li>
+      <li>
+        At Direct Hearing Aid, we are proud of our reputation, the skills of our
+        technicians, the quality of our hearing aid repair parts, and an
+        unparalleled customer service.
+      </li>
+    </ul>
+    <p>If you have any questions, don’t hesitate to give us a call!</p>
+  </div>
+);
 
-export default Lorem
+const Page3 = () => (
+  <div>
+    <h1>Page 3 / SERVICES</h1>
+    <p>INSERT FORM</p>
+    <div>
+      <h1>Page 4 / FORM CONT</h1>
+    </div>
+  </div>
+);
+
+const Services = () => (
+  <>
+    <div>
+      <h1>Page 5 - What is a Class One Digital and Power Repair?</h1>
+      <p>
+        <b>
+          {" "}
+          Class 1 Digital and Power Repair: An Explanation of this
+          Classification
+        </b>
+      </p>
+      <p>
+        A hearing aid consists of 2 basic component categories: the shell and
+        the electronic devices which includes one or more microphones
+        (transducer) which receive sound waves, a receiver (speaker) which
+        transmits or projects processed and modified sound, into the ear canal.
+      </p>
+      <p>Both of these type components are:</p>
+      <ul>
+        <li>
+          Very susceptible to contamination by dirt, dust, moisture, chemicals
+          (such as hair spray) and ear wax created within the ear canal.
+        </li>
+        <li>
+          Contain very delicate membranes which can be damaged by physical shock
+          or environmental factors, including humidity and moisture.
+        </li>
+        <li>
+          Some hearing aids include switches and other devices such as a
+          Telescopic (for use with a telephone) and
+        </li>
+      </ul>
+      <p>Such devices are included in this repair classification.</p>
+      <p>
+        The nature of ear wax, in conjunction with movement, expansion and
+        contraction of the ear canal, results in the ear wax moving within the
+        ear canal and getting into parts of the hearing aid, such as the
+        receiver tube. The slightest amount of such contaminants can obstruct
+        the passage of sound, and render the hearing aid ineffective.
+      </p>
+      <p>
+        Another electronic component within the hearing aid is a device called a
+        capacitor. The manufacturer of the capacitor has a chemical ingredient
+        which is subject to deterioration over time, and can become defective.
+      </p>
+      <p>
+        Other components are: resistors, which affect the flow of electrical
+        current, the amplifier, which is critical to the operation and function
+        of the hearing aid, and copper wiring which is attached by solder and
+        connects the components.
+      </p>
+      <p>
+        Accidental physical abuse, such as dropping a hearing aid on a hard
+        surface, can dislodge and damage the electronic components. A
+        malfunction in the tolerances of these components affects the
+        performance of the hearing aid.
+      </p>
+      <p>
+        Class 1 Hearing Aid Digital and Power Repair includes the repair or
+        replacement of any of these electronic components, BUT we maintain the
+        "original" hearing loss settings inside the Hearing Aid. Within the
+        warranty period provided with the repair of the hearing aid, all
+        components and labor are provided regardless of whether they were
+        functioning according to specification at the time of the initial repair
+        and were not the cause for the original failure.
+      </p>{" "}
+    </div>
+    <div>
+      <h1>What is Class 2 Hearing Aid Repair?</h1>
+      <p> Class 2 Hearing Aid Repair includes the following ouelet options:</p>
+      <ul>
+        <li>Class 1 (Basic Repair)</li>
+        <li>Re-installation of original electronics</li>
+        <li>
+          Replacement of a Faceplate and battery door because of damage or
+          defect
+        </li>
+      </ul>
+      <p>
+        Any of these changes offered are always provided at a much greater
+        savings than the purchase of a New Hearing Aid with the same or similar
+        benefits.
+      </p>
+      <p>
+        When the Faceplate is manufactured, what is called a Hinge Pin, which
+        allows for the attachment and functioning of a battery door, is molded
+        into the Faceplate. It infrequently becomes dislodged and cannot be
+        replaced.
+      </p>
+      <p>
+        If the Hinge Pin becomes dislodged or otherwise damaged, such a repair
+        involves the complete replacement of the Faceplate.
+      </p>
+      <p>
+        The faceplate may also have to be replaced due to Physical Damage or
+        Abuse, or from the plastic cracking or otherwise being damaged and
+        unserviceable.
+      </p>{" "}
+    </div>
+    <div>
+      <h1>What is Class 3 Hearing Aid Repair?</h1>
+      <p> Class 3 Hearing Aid Repair includes the following options:</p>
+      <ul>
+        <li>Class 1 (Basic Repair)</li>
+        <li>
+          Replacement of damge mold (shell) Current Impression needed (Using
+          exisiting faceplate and electronics)
+        </li>
+      </ul>
+      <p>
+        How is this done? A block (usually consisting of cotton or rubber) is
+        inserted into the ear canal (including a removal string), and through
+        the use of a syringe, the prepared material is injected into the canal
+        and concha, allowed time to harden, and is then removed.
+      </p>
+      <p>
+        The finished impression is then submitted to an earmold lab which begins
+        the process of manufacturing a shell by making a mold of the impression
+        and then casting a plastic replica of the ear. This becomes what is
+        called the shell of a Custom-Made Hearing Aid.
+      </p>
+      <p>
+        The faceplate is a separate pre-finished plastic part which is then
+        bonded to the finished shell. The faceplate, which holds the appropriate
+        electronic components including the battery door, microphone and volume
+        control, is not custom-made but is modified and adapted to fit the
+        custom shell.
+      </p>
+      <p>
+        The placement of all switches or buttons on a Faceplate is critical
+        since they have to be operated or activated by the use of a finger and
+        it is important that the hearing aid user can find and activate the
+        various devices by touch. The Replacement of such switches and devices
+        is usually accomplished under the Class 1 Electronic Repair Services.
+      </p>
+      <p>
+        The faceplate may also have to be replaced due to Physical Damage or
+        Abuse, or from the plastic cracking or otherwise being damaged and
+        unserviceable. A damaged faceplate prevents the separating and opening
+        of the faceplate from the shell, required in all Servicing of the
+        Hearing Aid itself.
+      </p>{" "}
+    </div>
+    <div>
+      <h1>What is Class 4 Hearing Aid Repair?</h1>
+      <p> Class 4 Hearing Aid Repair consist of the follong options:</p>
+      <ul>
+        <li>Upgrading electronics</li>
+        <li>Convert from analog to digital</li>
+        <li>Reprogram to current audiogram (Hearing Test)</li>
+        <li>Option to change battery size</li>
+      </ul>
+      <p>
+        Serious consideration should be given to all the options available in
+        the event the Faceplate has to be replaced.
+      </p>
+      <p>
+        Any time a new faceplate is required, for any reason, it is strongly
+        recommended that a current Audiogram (hearing test) be submitted with
+        the request for repair services.
+      </p>
+      <ul>
+        <li>An upgrade to the existing analog circuit (more gain)</li>
+        <li>
+          An upgrade to the existing analog circuit to programmable analog
+          signal processing
+        </li>
+        <li>
+          An upgrade to the existing digital circuit to a current digital
+          circuit
+        </li>
+        <li>They choose to because of a change in hearing loss</li>
+        <li>
+          The desire to make changes or modifications at their discretion for
+          improvement, convenience of usage, or better hearing aid performance.
+        </li>
+      </ul>
+      <p>
+        When a New Faceplate is installed, unless otherwise stated, all New
+        Electronics are installed, and the only part of the existing hearing aid
+        used is the Shell and usually the same serial number.
+      </p>
+      <p>
+        Installing a new faceplate provides the opportunity to change the
+        internal components of like type (either Analog or Digital) to
+        compensate or allow for more gain (power) or other features. Features
+        such as Switches or Buttons can be added or removed at the time of
+        Faceplate replacement.
+      </p>
+      <p>
+        Usually depending upon availability or costs, the user may choose to
+        upgrade the Circuit and Components to allow for changes in the
+        performance of the hearing aid through the use of a Computer. The user
+        may choose to upgrade the existing hearing aid’s electronics to Digital
+        Signal Processing for enhanced benefits to the existing hearing aid. The
+        user may also choose to upgrade the existing Digital Circuit for
+        improved hearing or tolerance, performance, or for more convenient and
+        inexpensive service.
+      </p>
+      <p>
+        The user, owner, or caretaker, may choose to change an existing private
+        label, or brand of hearing aid circuit, because of Limited and
+        Non-Competitive Service Availability.
+      </p>
+      <p>
+        In such an event, the hearing aid user has the option to choose to have
+        the existing electronics transferred from the damaged faceplate, and
+        reused, or to use this opportunity to upgrade the electronics to either
+        different and stronger components (to compensate for a change in hearing
+        loss), to add or remove aforementioned Switches and Features, or to
+        convert the existing hearing aid to Analog or Digital Signal Processing.
+      </p>
+      <p>
+        Since Brands such as all the major PRIVATE LABELS in the hearing aid
+        industry, do not make their PROPRIETY SOFTWARE available to non-member
+        Dealers, such hearing aids can only be serviced and programmed by the
+        AUTHORIZED FRANCHISE DEALERS and not the GENERAL PUBLIC.
+      </p>{" "}
+    </div>
+    <div>
+      <h1>What is Class 5 Hearing Aid Repair?</h1>
+      <p>Class 5 Hearing Aid Repair includes the following options:</p>
+
+      <ul>
+        <li>When the hearing aid is beyond repair (Damage Hearing aid)</li>
+        <li>Class 4 repair (Upgraded)</li>
+        <li>Replacement of damge mold (shell) Current Impression needed</li>
+        <li>Reprogram to current audiogram (Hearing Test)</li>
+      </ul>
+    </div>
+  </>
+);
+const Lorem = () => {
+  return (
+    <div style={{ paddingTop: "6rem", display: 'flex', flexDirection: 'column', gap: '2rem' }}>
+      <Page1 />
+      <Page2 />
+      <Page3 />
+      <Services />
+    </div>
+  );
+};
+
+export default Lorem;
